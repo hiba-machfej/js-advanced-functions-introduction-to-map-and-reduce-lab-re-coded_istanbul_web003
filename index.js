45a3011d6c29174b0c9b1d04b9f2889426353334
Hiba <hiba.mashfij@gmail.com>
2020-08-12 09:40:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+
+function mapToNegativize(array) {
+    const array2 = [];
+    array.forEach(element => array2.push(element * -1));
+    return array2;
+}
+
+
+function mapToNoChange(array) {
+    const array2 = [];
+    array.forEach(element => array2.push(element));
+    return array2;
+}
+
+function mapToDouble(array) {
+    const array2 = [];
+    array.forEach(element => array2.push(element * 2));
+    return array2;
+}
+
+function mapToSquare(array) {
+    const array2 = [];
+    array.forEach(element => array2.push(element * element));
+    return array2;
+}
+
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    for (var i = 0; i < sourceArray.length; i++) {
+        startingPoint += sourceArray[i];
+    }
+    return startingPoint;
+}
+
+function reduceToAllTrue(sourceArray) {
+    if (sourceArray.includes(false) ) {
+      return false;
+    }
+   else {
+     return true;
+   }
+ }
+
+
+
+ function reduceToAnyTrue(sourceArray) {
+    if (sourceArray.includes(true) ) {
+      return true;
+    }
+   else {
+     return false;
+   }
+ }
